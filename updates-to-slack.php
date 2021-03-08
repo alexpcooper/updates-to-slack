d04dae39997455ebd27892b183dbe2402dd1b49b
Alex Cooper <10476164+alexpcooper@users.noreply.github.com>
2021-03-08 21:26:57 +0000

    Version 1.4.2
    Tested with Wordpress 5.7

--- updates-to-slack.php
+++ updates-to-slack.php
@@ -3,7 +3,7 @@
  * Plugin Name:         Updates to Slack
  * Plugin URI:
  * Description:         Sends Slack alerts about WordPress core, plugin and theme updates
- * Version:             1.4.1
+ * Version:             1.4.2
  * Author:              Alex Cooper
  * Author URI:          https://alexpcooper.co.uk/
  * License:             GPL v2 or later
